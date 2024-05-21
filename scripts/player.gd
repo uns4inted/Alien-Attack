126c78fce60b1aac50cf0db8dba22fe9fbc881bd
Uns4inted <lalanskyl@gmail.com>
2024-05-21 13:39:50 +0300

    Add movement keybindings, implement player controls

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,2 +1,19 @@
 extends CharacterBody2D
 
+
+var speed = 300
+
+func _physics_process(delta):
+	
+	velocity = Vector2(0, 0) # reset velocity each frame
+
+	# Player controls
+	if Input.is_action_pressed("move_up"):
+		velocity.y = -speed
+	if Input.is_action_pressed("move_down"):
+		velocity.y = speed
+	if Input.is_action_pressed("move_right"):
+		velocity.x = speed
+	if Input.is_action_pressed("move_left"):
+		velocity.x = -speed
+	move_and_slide()
