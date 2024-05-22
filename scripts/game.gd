fe060c5fb0e87dcf40cc8e26194a83e87ad4ad88
Uns4inted <lalanskyl@gmail.com>
2024-05-22 16:54:51 +0300

    Add UI/HUD scene, add score display

--- scripts/game.gd
+++ scripts/game.gd
@@ -2,15 +2,18 @@ extends Node2D
 
 @export var lives = 3
 @export var score_for_kill = 100
-@export var damage_penalty = 1000
+@export var damage_penalty = 500
 var score = 0
 
 
 @onready var player = $Player
+@onready var hud = $UI/HUD
+
+func _ready():
+	update_score()
 
 func _on_death_zone_area_entered(area):
 	area.despawn()
-
 
 func _on_player_took_damage():
 	# penalty for being damaged
@@ -18,11 +21,11 @@ func _on_player_took_damage():
 		score -= (damage_penalty + score_for_kill) # also remove score for kill
 	elif (score < damage_penalty):
 		score = -score_for_kill # set score to 0 + remove score for kill
+	update_score()
 	
 	# live reduce and death
 	lives -= 1	
 	if (lives < 1):
-		print("player died")
 		player.die()
 
 
@@ -32,4 +35,7 @@ func _on_enemy_spawner_enemy_spawned(enemy_instance):
 
 func _on_enemy_died():
 	score += score_for_kill
-	print("score: " + str(score))
+	update_score()
+
+func update_score():
+		hud.set_score_label(score)
