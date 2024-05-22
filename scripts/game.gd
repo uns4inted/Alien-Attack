d36c7c5b5b9cbb5acad22242553ee328d83afdbc
Uns4inted <lalanskyl@gmail.com>
2024-05-22 17:10:24 +0300

    Add Player Lives display on HUD

--- scripts/game.gd
+++ scripts/game.gd
@@ -5,12 +5,12 @@ extends Node2D
 @export var damage_penalty = 500
 var score = 0
 
-
 @onready var player = $Player
 @onready var hud = $UI/HUD
 
 func _ready():
-	update_score()
+	update_score_label()
+	update_lives_label()
 
 func _on_death_zone_area_entered(area):
 	area.despawn()
@@ -21,10 +21,11 @@ func _on_player_took_damage():
 		score -= (damage_penalty + score_for_kill) # also remove score for kill
 	elif (score < damage_penalty):
 		score = -score_for_kill # set score to 0 + remove score for kill
-	update_score()
+	update_score_label()
 	
 	# live reduce and death
-	lives -= 1	
+	lives -= 1
+	update_lives_label()
 	if (lives < 1):
 		player.die()
 
@@ -35,7 +36,10 @@ func _on_enemy_spawner_enemy_spawned(enemy_instance):
 
 func _on_enemy_died():
 	score += score_for_kill
-	update_score()
+	update_score_label()
 
-func update_score():
+func update_score_label():
 		hud.set_score_label(score)
+
+func update_lives_label():
+	hud.set_lives_label(lives)
--- scripts/hud.gd
+++ scripts/hud.gd
@@ -1,6 +1,10 @@
 extends Control
 
 @onready var score = $Score
+@onready var lives = $LivesCount
 
 func set_score_label(new_score):
 	score.text = "SCORE: " + str(new_score)
+
+func set_lives_label(new_lives):
+	lives.text = str(new_lives)
